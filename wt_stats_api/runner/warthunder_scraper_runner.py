2849c981a2a3dfb9dd28bf0b3e022a868d3785e0
0xZTD <buyankov.ilya@gmail.com>
2025-07-23 11:11:52 +0300

    fix: run on server using xvfb

--- wt_stats_api/runner/warthunder_scraper_runner.py
+++ wt_stats_api/runner/warthunder_scraper_runner.py
@@ -3,7 +3,7 @@ from pyvirtualdisplay import Display
 from scraper.warthunder_scraper import run
 
 # Start virtual display
-display = Display(visible=1, size=(1920, 1080))
+display = Display(visible=0, size=(1920, 1080))
 display.start()
 run()
 display.stop()
