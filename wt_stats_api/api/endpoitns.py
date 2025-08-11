17a52ac5467f2ae8dbcdf0652cdd01a96df989d6
0xZTD <buyankov.ilya@gmail.com>
2025-08-11 09:19:25 +0300

    add: 500 error resp

--- wt_stats_api/api/endpoitns.py
+++ wt_stats_api/api/endpoitns.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter, HTTPException
 from wt_stats_api.runner import warthunder_scraper_runner
+from selenium.common.exceptions import WebDriverException
 
 router = APIRouter()
 
 
 @router.get("/search")
 def search_endpoint(q: str):
-    results = warthunder_scraper_runner.get_player_link(q)
+    try:
+        results = warthunder_scraper_runner.get_player_link(q)
+    except WebDriverException as e:
+        raise HTTPException(status_code=500, detail="Server error")
+
     if not results:
         raise HTTPException(status_code=404, detail="No results found")
     return {"results": results}
@@ -15,7 +20,11 @@ def search_endpoint(q: str):
 @router.get("/stats")
 def stats_endpoint(url: str):
     url = url.strip('"').replace("#", "%23")
-    results = warthunder_scraper_runner.get_user_stats_by_url(url)
+    try:
+        results = warthunder_scraper_runner.get_user_stats_by_url(url)
+    except WebDriverException as e:
+        raise HTTPException(status_code=500, detail="Server error")
+
     if not results:
         raise HTTPException(status_code=404, detail="No results found")
     return {"results": results}
